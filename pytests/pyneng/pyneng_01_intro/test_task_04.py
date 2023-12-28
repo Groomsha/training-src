c5445ff7aa40719bdc08728bfc1aeb74cfbd60f1
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2023-12-28 16:57:18 +0200

    add: 2 task and test [intro]

---
+++ pytests/pyneng/pyneng_01_intro/test_task_04.py
@@ -0,0 +1,5 @@
+from pyneng.pyneng_01_intro.task_04 import *
+
+
+def test_list_words():
+    assert words == ["line", "test", "column", "result"]
---
+++ pytests/pyneng/pyneng_01_intro/test_task_05.py
@@ -0,0 +1,5 @@
+from pyneng.pyneng_01_intro.task_05 import *
+
+
+def test_list_words(value=index):
+    assert words[value] == "column"
